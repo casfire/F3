980ff8188bac04f7aeb1634b8543936e016b58bc
Casfire <admin@casfire.com>
2015-11-18 22:26:03 +0100

    Static and Render textures

--- src/F3/Texture.cpp
+++ src/F3/Texture.cpp
@@ -6,53 +6,29 @@
 using namespace F3;
 using namespace GL;
 
-inline GLenum getFormat(std::size_t channels, bool compress) {
-	switch (channels) {
-	case 1: return compress ? GL_COMPRESSED_RED  : GL_RED;
-	case 2: return compress ? GL_COMPRESSED_RG   : GL_RG;
-	case 3: return compress ? GL_COMPRESSED_RGB  : GL_RGB;
-	case 4: return compress ? GL_COMPRESSED_RGBA : GL_RGBA;
-	default: throw Error("Invalid number of channels.", __FILE__, __LINE__);
+// Texture
+
+Texture::Texture(
+	std::size_t width,
+	std::size_t height,
+	std::size_t channels
+)
+: ID(0)
+, width(width)
+, height(height)
+, channels(channels)
+{
+	glGenTextures(1, &ID);
+	if (ID <= 0) {
+		throw Error("Failed to generate texture.", __FILE__, __LINE__);
+	} else if (channels > 4) {
+		throw Error("More than 4 channels.", __FILE__, __LINE__);
 	}
 }
 
-inline void setFilter(TextureFilter filter) {
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	switch (filter) {
-	case TextureFilter::NEAREST:
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);	
-		break;
-	case TextureFilter::BILINEAR:
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		break;
-	case TextureFilter::TRILINEAR:
-		glGenerateMipmap(GL_TEXTURE_2D);
-		glTexParameteri(
-			GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR
-		);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		break;
-	case TextureFilter::ANISOTROPIC:
-		glGenerateMipmap(GL_TEXTURE_2D);
-		glTexParameteri(
-			GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR
-		);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		if (ext_GL_EXT_texture_filter_anisotropic) {
-			float max;
-			glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &max);
-			glTexParameterf(
-				GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, max
-			);
-		}
-		break;
-	default:
-		throw Error("Invalid texture filter.", __FILE__, __LINE__);
-		break;
-	}
+Texture::~Texture()
+{
+	glDeleteTextures(1, &ID);
 }
 
 std::size_t Texture::getWidth() const
@@ -70,87 +46,117 @@ std::size_t Texture::getChannels() const
 	return channels;
 }
 
-Texture::Texture(
-	std::size_t width, std::size_t height, std::size_t channels,
-	const std::uint8_t* pixels, TextureFilter filter, bool compress
-) : ID(0), width(width), height(height), channels(channels) {
-	GLint  internal = getFormat(channels, compress);
-	GLenum external = getFormat(channels, false);
-	glGenTextures(1, &ID);
+// StaticTexture
+
+static const GLenum minFilter[] = {
+	GL_NEAREST,
+	GL_LINEAR,
+	GL_LINEAR_MIPMAP_LINEAR,
+	GL_LINEAR_MIPMAP_LINEAR,
+};
+
+static const GLenum magFilter[] = {
+	GL_NEAREST,
+	GL_LINEAR,
+	GL_LINEAR,
+	GL_LINEAR,
+};
+
+static const GLenum glFormat[] = {
+	GL_RED,  GL_RG,    GL_RGB,    GL_RGBA,
+	GL_R8,   GL_RG8,   GL_RGB8,   GL_RGBA8,
+	GL_R16,  GL_RG16,  GL_RGB16,  GL_RGBA16,
+	GL_R16F, GL_RG16F, GL_RGB16F, GL_RGBA16F,
+	GL_R32F, GL_RG32F, GL_RGB32F, GL_RGBA32F,
+};
+
+StaticTexture::StaticTexture(
+	std::size_t width,
+	std::size_t height,
+	std::size_t channels,
+	const std::uint8_t* pixels,
+	StaticTextureFilter filter
+)
+: Texture(width, height, channels)
+{
+	if (channels == 0) throw Error("Zero channels.", __FILE__, __LINE__);
 	glBindTexture(GL_TEXTURE_2D, ID);
 	glTexImage2D(
-		GL_TEXTURE_2D, 0, internal, width, height,
-		0, external, GL_UNSIGNED_BYTE, pixels
+		GL_TEXTURE_2D, 0, glFormat[channels - 1], width, height,
+		0, glFormat[channels - 1], GL_UNSIGNED_BYTE, pixels
 	);
-	setFilter(filter);
+	std::size_t f = static_cast<std::size_t>(filter);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter[f]);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter[f]);
+	if (f == 3 && ext_GL_EXT_texture_filter_anisotropic) {
+		float max;
+		glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &max);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, max);
+	}
 	glBindTexture(GL_TEXTURE_2D, 0);
 	if (glGetError() != GL_NO_ERROR) {
-		throw Error("Failed to create texture.", __FILE__, __LINE__);
+		throw Error("Failed to create static texture.", __FILE__, __LINE__);
 	}
 }
 
-Texture::Texture(
+StaticTexture::StaticTexture(
 	const std::string& filename,
-	TextureFilter filter, bool compress
-) : ID(0) {
+	StaticTextureFilter filter
+)
+: Texture(0, 0, 0)
+{
 	unsigned char *data; int w, h, c;
 	data = stbi_load(filename.c_str(), &w, &h, &c, 0);
 	if (data == nullptr) {
-		throw Exception(
+		throw IOException(
 			"Failed to load " + filename + ":\n"
 			+ std::string(stbi_failure_reason())
 		);
 	}
 	width = w; height = h; channels = c;
-	GLint  internal = getFormat(channels, compress);
-	GLenum external = getFormat(channels, false);
-	glGenTextures(1, &ID);
 	glBindTexture(GL_TEXTURE_2D, ID);
 	glTexImage2D(
-		GL_TEXTURE_2D, 0, internal, width, height,
-		0, external, GL_UNSIGNED_BYTE, data
+		GL_TEXTURE_2D, 0, glFormat[channels - 1], width, height,
+		0, glFormat[channels - 1], GL_UNSIGNED_BYTE, data
 	);
-	setFilter(filter);
+	std::size_t f = static_cast<std::size_t>(filter);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter[f]);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter[f]);
+	if (f == 3 && ext_GL_EXT_texture_filter_anisotropic) {
+		float max;
+		glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &max);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, max);
+	}
 	glBindTexture(GL_TEXTURE_2D, 0);
 	stbi_image_free(data);
 	if (glGetError() != GL_NO_ERROR) {
-		throw Error("Failed to create texture.", __FILE__, __LINE__);
+		throw Error("Failed to create static texture.", __FILE__, __LINE__);
 	}
 }
 
-Texture::Texture(
-	const void* memory, std::size_t length,
-	TextureFilter filter, bool compress
-) : ID(0) {
-	unsigned char *data; int w, h, c;
-	data = stbi_load_from_memory(
-		reinterpret_cast<const stbi_uc*>(memory),
-		length, &w, &h, &c, 0
-	);
-	if (data == nullptr) {
-		throw Exception(
-			"Failed to load texture:\n"
-			+ std::string(stbi_failure_reason())
-		);
-	}
-	width = w; height = h; channels = c;
-	GLint  internal = getFormat(channels, compress);
-	GLenum external = getFormat(channels, false);
-	glGenTextures(1, &ID);
-	glBindTexture(GL_TEXTURE_2D, ID);
-	glTexImage2D(
-		GL_TEXTURE_2D, 0, internal, width, height,
-		0, external, GL_UNSIGNED_BYTE, data
-	);
-	setFilter(filter);
-	glBindTexture(GL_TEXTURE_2D, 0);
-	stbi_image_free(data);
-	if (glGetError() != GL_NO_ERROR) {
-		throw Error("Failed to create texture.", __FILE__, __LINE__);
-	}
-}
+// RenderTexture
 
-Texture::~Texture()
+RenderTexture::RenderTexture(
+	std::size_t width,
+	std::size_t height,
+	std::size_t channels,
+	RenderTextureFilter filter,
+	RenderTextureFormat format
+)
+: Texture(width, height, channels)
 {
-	glDeleteTextures(1, &ID);
+	if (channels == 0) throw Error("Zero channels.", __FILE__, __LINE__);
+	glBindTexture(GL_TEXTURE_2D, ID);
+	glTexImage2D(
+		GL_TEXTURE_2D, 0,
+		glFormat[4 + static_cast<std::size_t>(format) * 4 + channels - 1],
+		width, height, 0, glFormat[channels - 1], GL_UNSIGNED_BYTE, nullptr
+	);
+	std::size_t f = static_cast<std::size_t>(filter);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter[f]);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter[f]);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	if (glGetError() != GL_NO_ERROR) {
+		throw Error("Failed to create render texture.", __FILE__, __LINE__);
+	}
 }
--- src/F3/Texture.hpp
+++ src/F3/Texture.hpp
@@ -7,36 +7,29 @@
 
 namespace F3 {
 	
-	enum class TextureFilter {
-		NEAREST,
-		BILINEAR,
-		TRILINEAR,
-		ANISOTROPIC,
+	enum class StaticTextureFilter {
+		NEAREST     = 0,
+		BILINEAR    = 1,
+		TRILINEAR   = 2,
+		ANISOTROPIC = 3,
+	};
+	
+	enum class RenderTextureFilter {
+		NEAREST  = 0,
+		BILINEAR = 1,
+	};
+	
+	enum class RenderTextureFormat {
+		U8  = 0,
+		U16 = 1,
+		F16 = 2,
+		F32 = 3,
 	};
 	
 	class Texture {
 	public:
 		
-		Texture(
-			std::size_t width, std::size_t height,
-			std::size_t channels, const std::uint8_t* pixels,
-			TextureFilter filter = TextureFilter::NEAREST,
-			bool compress = true
-		);
-		
-		Texture(
-			const std::string& filename,
-			TextureFilter filter = TextureFilter::NEAREST,
-			bool compress = true
-		);
-		
-		Texture(
-			const void* memory, std::size_t length,
-			TextureFilter filter = TextureFilter::NEAREST,
-			bool compress = true
-		);
-		
-		~Texture();
+		virtual ~Texture();
 		
 		std::size_t getWidth() const;
 		std::size_t getHeight() const;
@@ -44,11 +37,52 @@ namespace F3 {
 		
 	private:
 		
+		friend class StaticTexture;
+		friend class RenderTexture;
+		
 		Texture(const Texture&) = delete;
 		Texture& operator=(const Texture&) = delete;
+		Texture(
+			std::size_t width,
+			std::size_t height,
+			std::size_t channels
+		);
 		
 		unsigned int ID;
-		std::size_t width, height, channels;
+		std::size_t width;
+		std::size_t height;
+		std::size_t channels;
+		
+	};
+	
+	class StaticTexture : public Texture {
+	public:
+		
+		StaticTexture(
+			std::size_t width,
+			std::size_t height,
+			std::size_t channels,
+			const std::uint8_t* pixels,
+			StaticTextureFilter filter
+		);
+		
+		StaticTexture(
+			const std::string& filename,
+			StaticTextureFilter filter
+		);
+		
+	};
+	
+	class RenderTexture : public Texture {
+	public:
+		
+		RenderTexture(
+			std::size_t width,
+			std::size_t height,
+			std::size_t channels,
+			RenderTextureFilter filter,
+			RenderTextureFormat format
+		);
 		
 	};
 	
