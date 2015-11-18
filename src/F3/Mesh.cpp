2860976c1d5a9660aaa249975a01ef276282ef2e
Casfire <admin@casfire.com>
2015-11-18 22:30:42 +0100

    Code style

--- src/F3/Mesh.cpp
+++ src/F3/Mesh.cpp
@@ -5,10 +5,15 @@ using namespace F3;
 
 Mesh::Mesh(
 	const Attribute& position,
-	std::size_t start, std::size_t count,
+	std::size_t start,
+	std::size_t count,
 	MeshType type
-) : position(position), indices(nullptr),
-	start(start), count(count), type(type)
+)
+: position(position)
+, indices(nullptr)
+, start(start)
+, count(count)
+, type(type)
 {
 	if (count + start > position.buffer.getVertexCount()) {
 		throw Error("Invalid mesh range.", __FILE__, __LINE__);
@@ -18,10 +23,15 @@ Mesh::Mesh(
 Mesh::Mesh(
 	const Attribute& position,
 	const IndexBuffer& indices,
-	std::size_t start, std::size_t count,
+	std::size_t start,
+	std::size_t count,
 	MeshType type
-) : position(position), indices(&indices),
-	start(start), count(count), type(type)
+)
+: position(position)
+, indices(&indices)
+, start(start)
+, count(count)
+, type(type)
 {
 	if (count + start > indices.getCount()) {
 		throw Error("Invalid mesh range.", __FILE__, __LINE__);
@@ -31,8 +41,12 @@ Mesh::Mesh(
 Mesh::Mesh(
 	const Attribute& position,
 	MeshType type
-) : position(position), indices(nullptr),
-	start(0), count(position.buffer.getVertexCount()), type(type)
+)
+: position(position)
+, indices(nullptr)
+, start(0)
+, count(position.buffer.getVertexCount())
+, type(type)
 {
 	if (count + start > position.buffer.getVertexCount()) {
 		throw Error("Invalid mesh range.", __FILE__, __LINE__);
@@ -43,8 +57,12 @@ Mesh::Mesh(
 	const Attribute& position,
 	const IndexBuffer& indices,
 	MeshType type
-) : position(position), indices(&indices),
-	start(0), count(indices.getCount()), type(type)
+)
+: position(position)
+, indices(&indices)
+, start(0)
+, count(indices.getCount())
+, type(type)
 {
 	if (count + start > indices.getCount()) {
 		throw Error("Invalid mesh range.", __FILE__, __LINE__);
